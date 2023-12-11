d5e35618140460df5dea95ed5cfcdd1546aa1a88
Kirill <ynwa_2016@mail.ru>
2023-12-11 20:45:40 +0300

    new

--- main.py
+++ main.py
@@ -143,7 +143,7 @@ if winner != "Draw":
     columns = ['1', '2', '3', '4', '5', '6', '7', '8', '9', 'winner']
     data = [[position[0], position[1], position[2], position[3], position[4], position[5], position[6], position[7], position[8], winner]]
     df = pd.DataFrame(data, columns=columns)
-    df.to_csv('res.csv', mode='a', header=False)
+    df.to_csv('res.csv', mode='a', header=False, index=False)
 # print(board[0])
 # print(board[1])
 # print(board[2])
